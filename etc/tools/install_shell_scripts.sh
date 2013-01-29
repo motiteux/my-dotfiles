72c95714de6723ab4c74f34a127c58b57df745a8
Marc-Olivier Titeux <marcotit@localhost.localdomain>
2013-01-29 23:21:58 +0100

    Spring cleaning. Using dotfiles only now

--- etc/tools/install_shell_scripts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in $(ls shell);
-do
-    ln -sf $(pwd)/shell/$i bin/$i;
-    echo "Installed $i into bin/.";
-done;
--- install_shell_scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in $(ls -a $(pwd)/etc);
-do
-	if [ -e ~/$i ]; then 
-		rm -rf ~/$i
-	fi
-    ln -sf $(pwd)/etc/$i ~/$i;
-    echo "Installed $i into $HOME";
-done;
