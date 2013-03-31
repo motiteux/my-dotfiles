4ccf36279ca6cc5aa5f975597265d704e248b823
Marc-Olivier Titeux <Marco@marco-laptop.home>
2013-03-31 23:43:52 +0200

    Pruning useless files

--- bin/run_davmail.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /opt/software/davmail-linux-x86_64-4.1.0-2042/
-./davmail.sh
--- tools/launch_aster_monitoring.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gnome-terminal --title="Monitoring Aster computation" --geometry=80x18+1500+0 --hide-menubar --command="/home/irsrvhome1/R13/titeuxm/bin/log_cur_aster.sh $1 $2"
